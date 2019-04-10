8aaf3f19c3c1660e2ba7d68fc2c9c6436bcffea1
Krystian Zybala <kontakt@kzybala.pl>
2019-04-10 07:41:30 +0200

    Add spring application runner

---
+++ src/main/java/pl/krystianzybala/rcontract/RemoteContractApplication.java
@@ -0,0 +1,15 @@
+package pl.krystianzybala.rcontract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteContractApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemoteContractApplication.class, args);
+    }
+
+
+}
+
